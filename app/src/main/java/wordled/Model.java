391186d8a634b5b25c843c966b77562baf437281
Yves Kloeckner <hosta.gruen@gmail.com>
2025-08-26 21:07:26 -0600

    optimize pick best word

--- app/src/main/java/wordled/Model.java
+++ app/src/main/java/wordled/Model.java
@@ -2,18 +2,25 @@ package wordled;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Model implements WordleModel {
     private ArrayList<String> validWords;
     private String lastGuess;
     private static final String STARTING_GUESS = "tales";
+    private Random random;
 
     public Model() {
         this.validWords = new ArrayList<>();
         this.lastGuess = "";
+        this.random = new Random();
     }
 
     public int setInitialWords(File f) {
@@ -32,23 +39,91 @@ public class Model implements WordleModel {
         }
     }
 
+    public int characterDiversity(String s) {
+        Set<Character> seen = new HashSet<>();
+        int score = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!seen.contains(s.charAt(i))) {
+                score++;
+                seen.add(s.charAt(i));
+            }
+        }
+        return score;
+    }
+
+    private int[][] wordListFrequencies() {
+        int[][] freq = new int[26][5];
+        for (String w : this.validWords) {
+            for (int i = 0; i < 5; i++) {
+                char c = w.charAt(i);
+                freq[c - 'a'][i]++;
+            }
+        }
+        return freq;
+    }
+
+    private int[] maxFrequencyPerPosition(int[][] freq) {
+        int[] maxPos = new int[5];
+        for (int i = 0; i < 26; i++) {
+            for (int p = 0; p < 5; p++) {
+                if (freq[i][p] > maxPos[p])
+                    maxPos[p] = freq[i][p];
+            }
+        }
+        return maxPos;
+    }
+
+    private double scoreWord(String w, int[][] freqs, int[] maxPos) {
+        double score = 1.0;
+        for (int i = 0; i < 5; i++) {
+            char c = w.charAt(i);
+            int freq = (c >= 'a' && c <= 'z') ? freqs[c - 'a'][i] : 0;
+            int diff = freq - maxPos[i];
+            score *= (1.0 + (double) diff * diff);
+        }
+        score += this.random.nextDouble();
+        return score;
+    }
+
+    private String pickBestWord() {
+        if (this.validWords.isEmpty())
+            return null;
+        int[][] freq = wordListFrequencies();
+        int[] maxPos = maxFrequencyPerPosition(freq);
+
+        String best = this.validWords.get(0);
+        double bestScore = Double.POSITIVE_INFINITY;
+
+        for (String w : this.validWords) {
+            if (w.length() != 5)
+                continue;
+            double s = scoreWord(w, freq, maxPos);
+            if (s < bestScore) {
+                bestScore = s;
+                best = w;
+            }
+        }
+        return best;
+    }
+
     public String getGuess() {
-        System.out.println("pool size: " + String.valueOf(this.validWords.size()));
+        // System.out.println("pool size: " + String.valueOf(this.validWords.size()));
 
         if (this.lastGuess.equals("")) {
             this.lastGuess = Model.STARTING_GUESS;
             return Model.STARTING_GUESS;
         }
 
-        this.lastGuess = this.validWords.get(0);
-        this.validWords.remove(0);
+        this.lastGuess = this.pickBestWord();
+        this.validWords.remove(this.lastGuess);
         return this.lastGuess;
     }
 
     private int countOfCharInString(String s, char c) {
         int count = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == c) count++;
+            if (s.charAt(i) == c)
+                count++;
         }
         return count;
     }
